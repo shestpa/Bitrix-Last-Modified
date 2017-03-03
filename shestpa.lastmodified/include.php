03a643693c1a80e5dd639ecf61538a4c998dc5a1
Shestpa <shestpa@gmail.com>
2017-03-04 02:15:41 +0500

    Stub scripts

--- shestpa.lastmodified/include.php
+++ shestpa.lastmodified/include.php
@@ -1,2 +1,14 @@
 <?php
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+
+use Bitrix\Main\Loader;
+use Bitrix\Main\EventManager;
+
+Loader::registerAutoLoadClasses('shestpa.lastmodified', array(
+    // no thanks, bitrix, we better will use psr-4 than your class names convention
+    'Shestpa\Lastmodified\PagesTimestampTable' => 'lib/PagesTimestampTable.php',
+));
+
+EventManager::getInstance()->addEventHandler('main', 'OnAfterUserAdd', function(){
+    // do something when new user added
+});
--- shestpa.lastmodified/install/version.php
+++ shestpa.lastmodified/install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
 	"VERSION" => "0.0.1",
-	"VERSION_DATE" => "2017-03-04 01:47:00"
+	"VERSION_DATE" => "2017-04-03 02:00:00"
 );
 ?>
